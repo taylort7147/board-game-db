6508e80729ddb42ec094f3c193ae90fc38ec7383
taylort7147 <taylort7147@gmail.com>
2022-07-06 11:47:11 -0700

    Migration: InitialCreate

---
+++ BoardGameDB/Migrations/20220706184640_InitialCreate.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoardGameDB.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Location = table.Column<string>(type: "TEXT", maxLength: 8, nullable: false),
+                    Complexity = table.Column<int>(type: "INTEGER", nullable: true),
+                    MinimumPlayTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaximumPlayTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinimumPlayerCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaximumPlayerCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    PictureUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    RulesUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    RulesVideoUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    BoardGameGeekUrl = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayStyle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayStyle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    GameId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameType_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mechanic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    GameId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mechanic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mechanic_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameType_GameId",
+                table: "GameType",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mechanic_GameId",
+                table: "Mechanic",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameType");
+
+            migrationBuilder.DropTable(
+                name: "Mechanic");
+
+            migrationBuilder.DropTable(
+                name: "PlayStyle");
+
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}

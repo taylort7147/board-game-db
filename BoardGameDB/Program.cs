e2d127011c022dcd74ae508870d690c7477d0db0
taylort7147 <taylort7147@gmail.com>
2022-07-06 11:45:40 -0700

    Add SQLite

--- BoardGameDB/Program.cs
+++ BoardGameDB/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<BoardGameDB.Data.BoardGameDBContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
